da7836d56107353a56b3c54ce7d23ba3c4926f20
mdmn07C5 <malicdan@protonmail.ch>
2024-03-27 14:03:21 -0400

    Demos text/template lib

--- cmd/main.go
+++ cmd/main.go
@@ -3,9 +3,27 @@ package main
 import (
 	"go-templates/temp"
 	"go-templates/templates"
+	"log"
+	"os"
+	"text/template"
 )
 
 func main() {
 	rudimentaryTemplate := temp.CreateRudimentaryTemplate("Apu Apustaja")
 	templates.HTMLfromString(rudimentaryTemplate, "index", "")
+
+	tpl, err := template.ParseGlob("../templates/*.gohtml")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = tpl.ExecuteTemplate(os.Stdout, "template2.gohtml", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = tpl.ExecuteTemplate(os.Stdout, "template.gohtml", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
