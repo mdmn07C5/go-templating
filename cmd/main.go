8fe3658523b4e233d51d56aeddb7ea04123debed
mdmn07C5 <malicdan@protonmail.ch>
2024-03-27 15:08:50 -0400

    Messing around with passing functions to templates
    (This sort of violates MVC pattern)

--- cmd/main.go
+++ cmd/main.go
@@ -3,10 +3,24 @@ package main
 import (
 	"log"
 	"os"
+	"strings"
 	"text/template"
 )
 
 var tpl *template.Template
+
+var fm = template.FuncMap{
+	"lower": strings.ToLower,
+	"flip":  Flip,
+}
+
+func Flip(s string) string {
+	runes := []rune(s)
+	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
+		runes[i], runes[j] = runes[j], runes[i]
+	}
+	return string(runes)
+}
 
 type person struct {
 	Name   string
@@ -14,7 +28,7 @@ type person struct {
 }
 
 func init() {
-	tpl = template.Must(template.ParseGlob("../templates/*.gohtml"))
+	tpl = template.Must(template.New("").Funcs(fm).ParseGlob("../templates/*.gohtml"))
 }
 
 func main() {
@@ -25,18 +39,19 @@ func main() {
 		jej[lel[i]] = lel[n-i-1]
 	}
 
-	err := tpl.ExecuteTemplate(os.Stdout, "template-ranges-maps.gohtml", jej)
-	if err != nil {
-		log.Fatal(err)
-	}
-
 	dudes := []person{
 		{Name: "Apu", Height: 456},
 		{Name: "Apustaja", Height: 6546},
 	}
 
-	err = tpl.ExecuteTemplate(os.Stdout, "template-struct.gohtml", dudes)
+	err := tpl.ExecuteTemplate(os.Stdout, "template-struct.gohtml", dudes)
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	err = tpl.ExecuteTemplate(os.Stdout, "template-ranges-maps-funcs.gohtml", jej)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 }
