0939c887bdab848f51d41d48521b0b51070d5a20
mdmn07C5 <malicdan@protonmail.ch>
2024-03-27 14:48:18 -0400

    messing around with passing data to templates

--- cmd/main.go
+++ cmd/main.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"go-templates/temp"
-	"go-templates/templates"
 	"log"
 	"os"
 	"text/template"
@@ -10,20 +8,34 @@ import (
 
 var tpl *template.Template
 
+type person struct {
+	Name   string
+	Height int
+}
+
 func init() {
 	tpl = template.Must(template.ParseGlob("../templates/*.gohtml"))
 }
 
 func main() {
-	rudimentaryTemplate := temp.CreateRudimentaryTemplate("Apu Apustaja")
-	templates.HTMLfromString(rudimentaryTemplate, "index", "")
+	lel := []string{"LMAO", "PEEPEE", "POOPOO"}
+	jej := make(map[string]string)
+	n := len(lel)
+	for i := 0; i < n; i++ {
+		jej[lel[i]] = lel[n-i-1]
+	}
 
-	err := tpl.ExecuteTemplate(os.Stdout, "template2.gohtml", nil)
+	err := tpl.ExecuteTemplate(os.Stdout, "template-ranges-maps.gohtml", jej)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	err = tpl.ExecuteTemplate(os.Stdout, "template.gohtml", nil)
+	dudes := []person{
+		{Name: "Apu", Height: 456},
+		{Name: "Apustaja", Height: 6546},
+	}
+
+	err = tpl.ExecuteTemplate(os.Stdout, "template-struct.gohtml", dudes)
 	if err != nil {
 		log.Fatal(err)
 	}
