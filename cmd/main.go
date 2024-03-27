becc7673b6a2427e1d818bbbbe350817d525f283
mdmn07C5 <malicdan@protonmail.ch>
2024-03-27 14:07:28 -0400

    Moves creating tpl to init()

--- cmd/main.go
+++ cmd/main.go
@@ -8,16 +8,17 @@ import (
 	"text/template"
 )
 
+var tpl *template.Template
+
+func init() {
+	tpl = template.Must(template.ParseGlob("../templates/*.gohtml"))
+}
+
 func main() {
 	rudimentaryTemplate := temp.CreateRudimentaryTemplate("Apu Apustaja")
 	templates.HTMLfromString(rudimentaryTemplate, "index", "")
 
-	tpl, err := template.ParseGlob("../templates/*.gohtml")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	err = tpl.ExecuteTemplate(os.Stdout, "template2.gohtml", nil)
+	err := tpl.ExecuteTemplate(os.Stdout, "template2.gohtml", nil)
 	if err != nil {
 		log.Fatal(err)
 	}
